225ec85b7a36ae2b929c9ccd34dad061cd05101f
Sprasad <devsprasadbhat@gmail.com>
2014-03-09 11:03:23 +0530

    index.html -> index.php and bootstrap.php linked

---
+++ includes/config.php
@@ -0,0 +1,11 @@
+<?php 
+
+	define('SITE_TITLE', 'OpenCube');
+	define('SITE_SLOGAN','Inspiring and Promoting technologies');
+
+	define('DBHOST', 'localhost');
+	define('DBUSER', 'root');
+	define('DBPASSWD', 'spbjtechman');
+	define('DBNAME', 'OpenCube');
+	define('DB_CONFIG', 'configs');
+	
